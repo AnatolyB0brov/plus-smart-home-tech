1511f8f51376bd349ebd2a4678026040962985ac
AnatolyB0brov <anatoly.b0brov@yandex.ru>
2025-02-25 21:06:49 +0300

    Sprint №19

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/HubController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/HubController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.controller;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,13 @@ import ru.yandex.practicum.service.DeviceService;
 @RestController
 @RequestMapping("/events/hubs")
 @AllArgsConstructor
+@Slf4j
 public class HubController {
     private final DeviceService deviceService;
 
     @PostMapping
     public void collectHubEvent(@Valid @RequestBody DeviceEvent deviceEvent) {
-        System.out.println("MY LOG "+deviceEvent);
+        log.info("MY LOG {}",deviceEvent);
         deviceService.processDeviceEvent(deviceEvent);
     }
 }
