9c865b6aa6d7f1874b473a0589e4d6704832b0fc
AnatolyB0brov <anatoly.b0brov@yandex.ru>
2025-02-25 22:30:46 +0300

    Sprint №19

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/handler/GlobalExceptionHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/handler/GlobalExceptionHandler.java
@@ -3,13 +3,14 @@ package ru.yandex.practicum.controller.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(Exception.class)
-    public void handleException(Exception e) {
-        log.error("HANDLER {}", e.getMessage());
+    public void handleException(Exception e, WebRequest webRequest) {
+        String body = webRequest.getParameterMap().keySet().iterator().next();
+        log.error("HANDLER {} {}",body, e.getMessage());
     }
 }
