1a84a026c276a8b0e4339d0a08c769af5ad4c763
AnatolyB0brov <anatoly.b0brov@yandex.ru>
2025-02-25 22:36:33 +0300

    Sprint №19

--- telemetry/collector/src/main/java/ru/yandex/practicum/dto/hubs/DeviceAction.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/dto/hubs/DeviceAction.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.dto.hubs;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
