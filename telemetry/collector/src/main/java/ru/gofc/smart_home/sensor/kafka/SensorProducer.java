93ef776afb4fb94a5102a81edc643177788458fc
AnatolyB0brov <anatoly.b0brov@yandex.ru>
2025-03-06 21:00:20 +0300

    test

---
+++ telemetry/collector/src/main/java/ru/gofc/smart_home/sensor/kafka/SensorProducer.java
@@ -0,0 +1,29 @@
+package ru.gofc.smart_home.sensor.kafka;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Slf4j
+public class SensorProducer {
+    Producer<String, SensorEventAvro> producer;
+    final String topic;
+
+    public void sendMessage(SensorEventAvro eventAvro) {
+        log.info("Отправление события сенсора " + eventAvro.getId());
+        log.debug("Отправление события сенсора " + eventAvro);
+
+        ProducerRecord<String, SensorEventAvro> producerRecord = new ProducerRecord<>(topic, eventAvro);
+
+        producer.send(producerRecord);
+        producer.flush();
+
+        log.info("Успешно отправлено событие сенсора " + eventAvro.getId());
+    }
+}
--- telemetry/collector/src/main/java/ru/yandex/practicum/handler/hubs/HubHandler.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.handler.hubs;
-
-import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
-
-public interface HubHandler {
-    HubEventProto.PayloadCase getMessageType();
-     void handle(HubEventProto eventProto);
-}
